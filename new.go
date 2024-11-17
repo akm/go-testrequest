b47a436a86e3b4e8a31aa1e4ad34a78903bb7308
akm <akm2000@gmail.com>
2024-11-17 22:58:53 +0900

    ✨ Add functions for HTTP method

--- new.go
+++ new.go
@@ -9,3 +9,13 @@ func New(method string, options ...Option) *http.Request {
 	}
 	return b.build()
 }
+
+func GET(v ...Option) *http.Request     { return New(http.MethodGet, v...) }
+func HEAD(v ...Option) *http.Request    { return New(http.MethodHead, v...) }
+func POST(v ...Option) *http.Request    { return New(http.MethodPost, v...) }
+func PUT(v ...Option) *http.Request     { return New(http.MethodPut, v...) }
+func PATCH(v ...Option) *http.Request   { return New(http.MethodPatch, v...) }
+func DELETE(v ...Option) *http.Request  { return New(http.MethodDelete, v...) }
+func CONNECT(v ...Option) *http.Request { return New(http.MethodConnect, v...) }
+func OPTIONS(v ...Option) *http.Request { return New(http.MethodOptions, v...) }
+func TRACE(v ...Option) *http.Request   { return New(http.MethodTrace, v...) }
