e025594dea424bb6706d87ae3dc65f6ab8ff611b
akm <akm2000@gmail.com>
2024-11-19 08:11:33 +0900

    💡 Add comments

--- func.go
+++ func.go
@@ -5,4 +5,5 @@ import (
 	"testing"
 )
 
+// Func is a type alias for a function that takes a *testing.T and returns an *http.Request.
 type Func = func(*testing.T) *http.Request
--- new.go
+++ new.go
@@ -1,15 +1,33 @@
 package testrequest
 
+// New creates a new request with the specified method and options.
 func New(method string, options ...Option) Func {
 	return DefaultFactory.New(method, options...)
 }
 
-func GET(v ...Option) Func     { return DefaultFactory.GET(v...) }
-func HEAD(v ...Option) Func    { return DefaultFactory.HEAD(v...) }
-func POST(v ...Option) Func    { return DefaultFactory.POST(v...) }
-func PUT(v ...Option) Func     { return DefaultFactory.PUT(v...) }
-func PATCH(v ...Option) Func   { return DefaultFactory.PATCH(v...) }
-func DELETE(v ...Option) Func  { return DefaultFactory.DELETE(v...) }
+// GET creates a new GET request with the specified options.
+func GET(v ...Option) Func { return DefaultFactory.GET(v...) }
+
+// HEAD creates a new HEAD request with the specified options.
+func HEAD(v ...Option) Func { return DefaultFactory.HEAD(v...) }
+
+// POST creates a new POST request with the specified options.
+func POST(v ...Option) Func { return DefaultFactory.POST(v...) }
+
+// PUT creates a new PUT request with the specified options.
+func PUT(v ...Option) Func { return DefaultFactory.PUT(v...) }
+
+// PATCH creates a new PATCH request with the specified options.
+func PATCH(v ...Option) Func { return DefaultFactory.PATCH(v...) }
+
+// DELETE creates a new DELETE request with the specified options.
+func DELETE(v ...Option) Func { return DefaultFactory.DELETE(v...) }
+
+// CONNECT creates a new CONNECT request with the specified options.
 func CONNECT(v ...Option) Func { return DefaultFactory.CONNECT(v...) }
+
+// OPTIONS creates a new OPTIONS request with the specified options.
 func OPTIONS(v ...Option) Func { return DefaultFactory.OPTIONS(v...) }
-func TRACE(v ...Option) Func   { return DefaultFactory.TRACE(v...) }
+
+// TRACE creates a new TRACE request with the specified options.
+func TRACE(v ...Option) Func { return DefaultFactory.TRACE(v...) }
